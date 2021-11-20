8bbfc6c31e8f6eacae38a175b0810f73d3def113
DarkLord <dfhrhfan12@yandex.ru>
2021-11-20 13:03:00 +0400

    Remove part funcs

--- Task-09_JS-JS-&-DOM/functions_lib.js
+++ Task-09_JS-JS-&-DOM/functions_lib.js
@@ -1,56 +1,4 @@
-const TASK_FUNCS_6 = {
-    task1(input) {
-        let result = [];
-        for (const item of input) {
-            result.push(...item);
-        }
-        return result.sort()
-    },
-    task2(input) {
-        return input.split('').map(char => {
-            let res = char.charCodeAt(0).toString(2);
-            if(res.length < 8) {
-                res = '0'.repeat(8 - res.length) + res;
-            }
-            return res;
-        })
-    },
-    task3(input) {
-        return [...input.matchAll(/[aeiou]/gi)].length;
-    },
-    task4(input) {
-        function* letterGeneartor(letter, count) {
-            let upper = letter.toUpperCase();
-            let lower = letter.toLowerCase();
-            yield upper;
-            for (let i = 1; i < count; i++) {
-                yield lower;
-            }
-        }
-
-        let words = [];
-        for (let letterIndex = 0; letterIndex < input.length; letterIndex++) {
-            const letter = input[letterIndex];
-            const count = letterIndex + 1;
-            let word = [...letterGeneartor(letter, count)].join('');
-            words.push(word);
-        }
-        return words.join('-');
-    },
-    task5(input) {
-        let arr = input.toLowerCase().split('');
-        return arr.every((char, index) => index == arr.indexOf(char));
-    },
-};
-
 const TASK_FUNCS_7 = {
-    task1(input) {
-        return !Array.isArray(input)
-            || input.length != 10
-            || !input.every(value => Number.isInteger(value) && value >= 0)
-            ? 'Invalid input'
-            : `+7 ${'(012) 345-67-89'.replace(/\d/g, letter => input[letter])}`;
-    },
     task2(input, isText) {
         if (!input || !input.w || !Number.isInteger(input.w) || input.w < 1 || !input.h || !Number.isInteger(input.h) || input.h < 1) {
             return 'Invalid input';
@@ -122,51 +70,6 @@ const TASK_FUNCS_7 = {
 };
 
 const TASK_FUNCS_8 = {
-    task1(input) {
-        function randInt(min, max) {
-            return min + (Math.random() * (max - min)) | 0;
-        }
-
-        function getRandomValue(values) {
-            let index = randInt(0, values.length);
-            return values[index];
-        }
-        const numbers = '0123456789';
-        const lettersLow = 'abcdefghijklmnopqrstuvwxyz';
-        const lettersUp = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
-        const resultLength = randInt(6, 20 + 1);
-
-        let res = getRandomValue(numbers) + getRandomValue(lettersLow) + getRandomValue(lettersUp);
-        while (res.length < resultLength) {
-            let variant = randInt(1, 3);
-            switch (variant) {
-                case 1:
-                    res += getRandomValue(numbers);
-                    break;
-                case 2:
-                    res += getRandomValue(lettersLow);
-                    break;
-                case 3:
-                    res += getRandomValue(lettersUp);
-                    break;
-            }
-        }
-
-        return res;
-    },
-    task2(input) {
-        let queue = Array.from({ length: input.count }, (_, i) => i + 1);
-        for (let step = 1; queue.length > 1; step++) {
-            let cur = queue.shift();
-            if (step % input.step == 0) {
-                step = 0;
-                continue;
-            }
-            queue.push(cur);
-        }
-        return queue.shift();
-    },
     task3(input) {
         const getOperator = operator => numberRight => numberLeft => operator(numberLeft, numberRight);
         const getNumber = value => operator => operator ? operator(value) : value;
--- Task-09_JS-JS-&-DOM/script.js
+++ Task-09_JS-JS-&-DOM/script.js
@@ -154,56 +154,6 @@ const pageElement = {
     tag: HTMLTags.Table,
     type: ItemTypes.Container,
     childs: [
-        // --- --- --- --- --- --- ---
-        // --- --- ---  6  --- --- ---
-        // --- --- --- --- --- --- ---
-        getTextLine('6.1. Плоский массив'),
-        getTaskValues(TASK_FUNCS_6.task1, [
-            [[3, 2, 1], [4, 6, 5], [], [9, 7, 8]],
-            [],
-            [[], []],
-            [[], [1]],
-            [[1, 3, 5], [-100], [2, 4, 6]],
-        ], JSON.stringify([[3, 2, 1], [4, 6, 5], [], [9, 7, 8]])),
-        getTextLine('6.2. Бинарное слово'),
-        getTaskValues(TASK_FUNCS_6.task2, [
-            'man',
-            'AB',
-            'wecking',
-            'Ruby',
-            'Yosemite',
-        ], '"man"'),
-        getTextLine('6.3. Подсчёт гласных'),
-        getTaskValues(TASK_FUNCS_6.task3, [
-            'abracadabra',
-            'ABRACADABRA',
-            'o a kak ushakov lil vo kashu kakao',
-            'my pyx'
-        ], '"abracadabra"'),
-        getTextLine('6.4. Форматирование строки'),
-        getTaskValues(TASK_FUNCS_6.task4, [
-            "abcd",
-            "RqaEzty",
-            "cwAt"
-        ], '"cat"'),
-        getTextLine('6.5. Изограммы'),
-        getTaskValues(TASK_FUNCS_6.task5, [
-            "Dermatoglyphics",
-            "aba",
-            "moOse",
-            ""
-        ], '"Dermatoglyphics"'),
-        // --- --- --- --- --- --- ---
-        // --- --- ---  7  --- --- ---
-        // --- --- --- --- --- --- ---
-        getTextLine('7.1. Форматирование телефонного номера'),
-        getTaskValues(TASK_FUNCS_7.task1, [
-            [9, 0, 0, 1, 1, 1, 2, 2, 3, 3],
-            [9, 2, 7, 5, 5, 5, 6, 6, 9, 0],
-            [1, 2, 3, 4, 5, 6, 7, 8, 9, -11],
-            [],
-            'aw93fha=',
-        ], [9, 2, 7, 5, 5, 5, 6, 6, 9, 0]),
         getTextLine('7.2. Форматирование строки в таблицу'),
         getTaskValues(TASK_FUNCS_7.task2, [
             { w: 4, h: 4, text: 'Hello World!' },
@@ -217,19 +167,6 @@ const pageElement = {
         ], JSON.stringify({ w: 3, h: 4, text: 'Nice long pattern' })),
         getTextLine('7.3. Формула для (a+b)^n'),
         getTaskValues(TASK_FUNCS_7.task3, [0, 1, 2, -2, 3, 5, 201, 3.14, ], 5),
-        // --- --- --- --- --- --- ---
-        // --- --- ---  8  --- --- ---
-        // --- --- --- --- --- --- ---
-        getTextLine('8.1. Генератор паролей'),
-        getTaskValues(TASK_FUNCS_8.task1, [
-            'none', 'none', 'none', 'none', 'none',
-        ], '"none"'),
-        getTextLine('8.2. Игра на выбывание'),
-        getTaskValues(TASK_FUNCS_8.task2, [
-            { count: 7, step: 3 },
-            { count: 11, step: 19 },
-            { count: 1, step: 300 },
-        ], JSON.stringify({ count: 7, step: 3 })),
         getTextLine('8.3. Калькулятор из функций'),
         getTaskValues(TASK_FUNCS_8.task3, [
             "seven(times(five())); // должно вернуть 35",
