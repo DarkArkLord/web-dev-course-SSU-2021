e38901968df05dde278cebfaa849d098cf8163b4
DarkLord <dfhrhfan12@yandex.ru>
2022-01-23 23:31:44 +0400

    Add controls

---
+++ Task-10_Browser-game/src/js/components/controls.js
@@ -0,0 +1,43 @@
+export const ConstrolKeys = {
+    Use: [
+        69, // E
+        13, // Enter
+        32, // Space
+        105 // Num9
+    ],
+    Back: [
+        81, // Q
+        27, // Esc
+        8, // Backspace
+        103 // Num7
+    ],
+    Up: [
+        87, // W
+        38, // Up
+        104 // Num8
+    ],
+    Right: [
+        68, // D
+        39, // Right
+        102 // Num6
+    ],
+    Down: [
+        83, // S
+        40, // Down
+        101 // Num5
+    ],
+    Left: [
+        65, // A
+        37, // Left
+        100 // Num4
+    ],
+};
+
+export const Commands = {
+    Up: 'UP',
+    Right: 'RIGHT',
+    Down: 'DOWN',
+    Left: 'LEFT',
+    Use: 'USE',
+    Back: 'BACK',
+};
