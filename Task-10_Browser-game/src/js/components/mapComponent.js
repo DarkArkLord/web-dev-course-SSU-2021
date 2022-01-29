5c83cb7dc140b96a74523406477e7edae5a83120
DarkLord <dfhrhfan12@yandex.ru>
2022-01-29 16:50:15 +0400

    Add component template

---
+++ Task-10_Browser-game/src/js/components/mapComponent.js
@@ -0,0 +1,12 @@
+import { HTMLTags, ItemTypes } from './render.js'
+import { Commands } from './controls.js'
+
+const CellContent = {
+    Player: '@',
+    Empty: '.',
+    Wall: '#'
+}
+
+export function MapComponent() {
+    //
+}
