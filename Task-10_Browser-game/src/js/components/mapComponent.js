6d457269642faadb764a5301c629af756ba1900a
DarkLord <dfhrhfan12@yandex.ru>
2022-01-29 16:50:25 +0400

    Upd cell content

--- Task-10_Browser-game/src/js/components/mapComponent.js
+++ Task-10_Browser-game/src/js/components/mapComponent.js
@@ -2,10 +2,49 @@ import { HTMLTags, ItemTypes } from './render.js'
 import { Commands } from './controls.js'
 
 const CellContent = {
-    Player: '@',
-    Empty: '.',
-    Wall: '#'
-}
+    Player: {
+        Value: '@',
+        Class: '',
+    },
+    Cell: {
+        Empty: {
+            Value: '.',
+            Class: '',
+        },
+        Wall: {
+            Value: '#',
+            Class: '',
+        },
+        Invisible: {
+            Value: '&nbsp;',
+            Class: '',
+        },
+    },
+    Flag: {
+        Normal: {
+            Value: 'F',
+            Class: '',
+        },
+        Used: {
+            Value: '!',
+            Class: '',
+        },
+    },
+    Door: {
+        Next: {
+            Value: '>',
+            Class: '',
+        },
+        Prev: {
+            Value: '<',
+            Class: '',
+        },
+        CLosed: {
+            Value: 'X',
+            Class: '',
+        },
+    },
+};
 
 export function MapComponent() {
     //
