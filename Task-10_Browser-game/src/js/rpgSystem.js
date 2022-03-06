71dd01b099f3fd0e5e02ff4574c44d503f260aae
DarkLord <dfhrhfan12@yandex.ru>
2022-03-07 01:23:08 +0400

    Add dmg by str

---
+++ Task-10_Browser-game/src/js/rpgSystem.js
@@ -0,0 +1,16 @@
+export function getDamageByStrength(strength) {
+    const baseStrength = 10;
+    const modToDice = 4;
+    let result = { dices: 1, mod: 0 };
+    let delta = strength - baseStrength;
+    if(delta < 0) {
+        result.mod = Math.sign(delta) * Math.round(Math.abs(delta) / 2);
+    } else {
+        result.mod = delta;
+        while(result.mod > 2) {
+            result.dices += 1;
+            result.mod -= modToDice;
+        }
+    }
+    return result;
+}
