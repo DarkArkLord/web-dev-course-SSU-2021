12f9887f6d6caf8497a70adf7c9844fb4cae3fc4
DarkLord <dfhrhfan12@yandex.ru>
2022-03-07 17:37:08 +0400

    Upd format

--- Task-10_Browser-game/src/js/rpgSystem.js
+++ Task-10_Browser-game/src/js/rpgSystem.js
@@ -3,11 +3,11 @@ export function getDamageByStrength(strength) {
     const modToDice = 4;
     let result = { dices: 1, mod: 0 };
     let delta = strength - baseStrength;
-    if(delta < 0) {
+    if (delta < 0) {
         result.mod = Math.sign(delta) * Math.round(Math.abs(delta) / 2);
     } else {
         result.mod = delta;
-        while(result.mod > 2) {
+        while (result.mod > 2) {
             result.dices += 1;
             result.mod -= modToDice;
         }
