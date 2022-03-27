83f028d30a47d9805363e60a507ae8211e131a41
DarkLord <dfhrhfan12@yandex.ru>
2022-03-27 12:17:44 +0400

    Add controls

---
+++ Task-10_Browser-game/src/ts/controls.ts
@@ -0,0 +1,44 @@
+export enum Commands {
+    Up = 'UP',
+    Down = 'DOWN',
+    Right = 'RIGHT',
+    Left = 'LEFT',
+    Use = 'USE',
+    Back = 'BACK',
+    Other = 'OTHER',
+};
+
+export const ConstrolKeys = {
+    [Commands.Use]: [
+        'KeyE',
+        'Enter',
+        'Space',
+        'Numpad9'
+    ],
+    [Commands.Back]: [
+        'KeyQ',
+        'Escape',
+        'Backspace',
+        'Numpad7'
+    ],
+    [Commands.Up]: [
+        'KeyW',
+        'ArrowUp',
+        'Numpad8'
+    ],
+    [Commands.Down]: [
+        'KeyS',
+        'ArrowDown',
+        'Numpad5'
+    ],
+    [Commands.Right]: [
+        'KeyD',
+        'ArrowRight',
+        'Numpad6'
+    ],
+    [Commands.Left]: [
+        'KeyA',
+        'ArrowLeft',
+        'Numpad4'
+    ],
+};
