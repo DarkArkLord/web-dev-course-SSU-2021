28feceee77ff5bdd53a66bc5c5baf41b299eb656
DarkLord <dfhrhfan12@yandex.ru>
2022-03-27 11:49:58 +0400

    Add demo

--- Task-10_Browser-game/src/ts/app.ts
+++ Task-10_Browser-game/src/ts/app.ts
@@ -1,2 +1,2 @@
 import '../scss/styles.scss';
-import './test'
+import './demo'
---
+++ Task-10_Browser-game/src/ts/demo.tsx
@@ -0,0 +1,15 @@
+function demo() {
+    return `        Hello world!
+      If you see this
+ init scripts work normaly
+           ______
+          / _  _ \\
+         |  O  O  |
+          \\ v--v /
+           \\____/
+            |  |
+           / \\/ \\
+          /  ||  \\`;
+}
+
+console.log(demo());
