b3c93171003f28db24c830424cb7cb6628e7b577
DarkLord <dfhrhfan12@yandex.ru>
2022-03-29 22:22:29 +0400

    Add base controller

---
+++ Task-10_Browser-game/src/ts/controllers/components/baseController.ts
@@ -0,0 +1,36 @@
+import { Commands } from "../../controls";
+
+declare type TCommandActions = {
+    [command in Commands]: () => void;
+};
+
+export abstract class BaseController implements IController {
+    globalController: IGlobalController;
+    commandActions: TCommandActions;
+
+    constructor() {
+        this.commandActions = {
+            [Commands.Up]: undefined,
+            [Commands.Down]: undefined,
+            [Commands.Use]: undefined,
+            [Commands.Left]: undefined,
+            [Commands.Right]: undefined,
+            [Commands.Back]: undefined,
+        };
+    }
+
+    onPush(globalController: IGlobalController): void {
+        this.globalController = globalController;
+    }
+    onPop(): void {
+        //
+    }
+
+    executeCommand(command: Commands): void {
+        let action = this.commandActions[command];
+        if (action) {
+            action();
+        }
+    }
+    abstract createElement(): HTMLElement;
+}
