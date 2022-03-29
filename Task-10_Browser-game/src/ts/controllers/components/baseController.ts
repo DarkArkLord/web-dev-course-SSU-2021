facf61541dc7826d2318a559604a6ca3e06a429d
DarkLord <dfhrhfan12@yandex.ru>
2022-03-29 22:40:45 +0400

    Remove custom init

--- Task-10_Browser-game/src/ts/controllers/components/baseController.ts
+++ Task-10_Browser-game/src/ts/controllers/components/baseController.ts
@@ -19,13 +19,8 @@ export abstract class BaseController implements IController {
         };
     }
 
-    customInit(): void { }
-
     onPush(globalController: IGlobalController): void {
         this.globalController = globalController;
-        if (this.customInit) {
-            this.customInit();
-        }
     }
     onPop(): void {
         //
