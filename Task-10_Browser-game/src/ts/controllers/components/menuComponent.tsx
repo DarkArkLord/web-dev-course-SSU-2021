5e709bea1b5273108537769a9c2c8c5bea78e8ee
DarkLord <dfhrhfan12@yandex.ru>
2022-03-29 23:56:37 +0400

    Add menu component

---
+++ Task-10_Browser-game/src/ts/controllers/components/menuComponent.tsx
@@ -0,0 +1,130 @@
+import { BaseController } from "./baseController";
+import { NumberEnumerator } from "../../utils/common";
+import { Commands } from "../../controls";
+
+const CSS = {
+    table: 'width_100 align_center',
+    item: {
+        default: 'menu-default-item',
+        current: 'menu-current-item',
+        disable: 'menu-disable-item',
+    },
+};
+
+type TMenuItem = {
+    value: string,
+    isActive: () => boolean,
+};
+
+type TMenuItemsActions = {
+    [value: string]: () => void
+};
+
+type TMenuItems = {
+    list: TMenuItem[],
+    actions: TMenuItemsActions,
+}
+
+function getItemClass(item: TMenuItem, index: number, enumerator: NumberEnumerator) {
+    let classes = [
+        item.isActive()
+            ? CSS.item.default
+            : CSS.item.disable
+    ];
+
+    if (index == enumerator.current()) {
+        classes.push(CSS.item.current);
+    }
+
+    return classes.join(' ');
+}
+
+export class MenuComponent extends BaseController {
+    header: HTMLElement;
+    footer: HTMLElement;
+    items: TMenuItems;
+    currentItem: NumberEnumerator;
+
+    constructor(items: TMenuItem[], header?: HTMLElement, footer?: HTMLElement) {
+        super();
+        let instance = this;
+
+        this.header = header;
+        this.footer = footer;
+
+        this.items = {
+            list: items,
+            actions: {}
+        };
+
+        this.currentItem = new NumberEnumerator(0, items.length - 1, 0);
+
+        this.commandActions[Commands.Up] = function () {
+            instance.currentItem.prev();
+        };
+        this.commandActions[Commands.Down] = function () {
+            instance.currentItem.next();
+        };
+        this.commandActions[Commands.Use] = function () {
+            let currentIndex = instance.currentItem.current();
+            let item = instance.items.list[currentIndex];
+            if (item && item.isActive()) {
+                let action = instance.items.actions[item.value];
+                if (action) {
+                    action();
+                }
+            }
+        };
+    }
+
+    onPush(globalController: IGlobalController): void {
+        super.onPush(globalController);
+        this.currentItem.reset();
+    }
+
+    createElement(): HTMLElement {
+        let instance = this;
+
+        function HeaderComponent() {
+            if (instance.header) {
+                return <tr>
+                    <td>
+                        {instance.header}
+                    </td>
+                </tr>;
+            }
+            return <></>
+        }
+
+        function ItemsListComponent() {
+            return <tr>
+                <td>
+                    <ul>
+                        {instance.items.list.map((item, index) => {
+                            let elementClass = getItemClass(item, index, instance.currentItem);
+                            let element = <li class={elementClass}>{item.value}</li>;
+                            return element;
+                        })}
+                    </ul>
+                </td>
+            </tr>;
+        }
+
+        function FooterComponent() {
+            if (instance.footer) {
+                return <tr>
+                    <td>
+                        {instance.footer}
+                    </td>
+                </tr>;
+            }
+            return <></>
+        }
+
+        return <table class={CSS.table}>
+            <HeaderComponent />
+            <ItemsListComponent />
+            <FooterComponent />
+        </table>;
+    }
+}
