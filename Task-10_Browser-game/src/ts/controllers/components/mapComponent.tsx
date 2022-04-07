5834a4d4fae9affbbcfacb2a90cf97f1a5066e9a
DarkLord <dfhrhfan12@yandex.ru>
2022-04-07 19:08:58 +0400

    Add map generator

--- Task-10_Browser-game/src/ts/controllers/components/mapComponent.tsx
+++ Task-10_Browser-game/src/ts/controllers/components/mapComponent.tsx
@@ -1,5 +1,6 @@
 import { BaseController } from "./baseController";
 import { Commands } from "../../controls";
+import { getRandomInt } from "../../utils/random";
 
 const CSS = {
     table: {
@@ -78,6 +79,80 @@ const CellContent = {
     },
 };
 
+type TPoint = { x: number, y: number };
+
+type TFlag = { position: TPoint, used: boolean };
+
+type TMap = {
+    map: string[][],
+    position: TPoint,
+    flags: { notUsedCount: number, list: TFlag[] },
+    doors: {
+        prev: { position: TPoint, isOpen: boolean },
+        next: { position: TPoint, isOpen: boolean },
+    }
+};
+
+function testMapGenerator(width: number, height: number, params: any): TMap {
+    let result: TMap = {
+        map: new Array(height),
+        position: { x: 0, y: 0 },
+        flags: { notUsedCount: params.flagCount, list: [] },
+        doors: {
+            prev: { position: null, isOpen: true },
+            next: { position: null, isOpen: false }
+        },
+    };
+
+    for (let y = 0; y < height; y++) {
+        result.map[y] = new Array(width);
+        for (let x = 0; x < width; x++) {
+            result.map[y][x] = CellType.Cell.Empty;
+        }
+        result.map[y][0] = CellType.Cell.Wall;
+        result.map[y][width - 1] = CellType.Cell.Wall;
+    }
+
+    for (let x = 0; x < width; x++) {
+        result.map[0][x] = CellType.Cell.Wall;
+        result.map[height - 1][x] = CellType.Cell.Wall;
+    }
+
+    const wallCount = Math.round((height + width) / 2);
+    for (let i = 0; i < wallCount; i++) {
+        let x = getRandomInt(1, width - 1);
+        let y = getRandomInt(1, height - 1);
+        result.map[y][x] = CellType.Cell.Wall;
+    }
+
+    function findFreeCell() {
+        while (true) {
+            let x = getRandomInt(1, width - 2);
+            let y = getRandomInt(1, height - 2);
+            if (result.map[y][x] == CellType.Cell.Empty) {
+                return { x, y };
+            }
+        }
+    }
+    result.position = findFreeCell();
+
+    result.doors.prev.position = { x: result.position.x, y: result.position.y };
+    result.map[result.position.y][result.position.x] = CellType.Door.Prev;
+
+    for (let i = 0; i < params.flagCount; i++) {
+        let flag = { position: findFreeCell(), used: false };
+        result.map[flag.position.y][flag.position.x] = CellType.Flag.NotUsed;
+        result.flags.list.push(flag);
+    }
+
+    result.doors.next.position = findFreeCell();
+    result.doors.next.isOpen = params.flagCount > 0;
+    result.map[result.doors.next.position.y][result.doors.next.position.x] = params.flagCount > 0
+        ? CellType.Door.Closed
+        : CellType.Door.Next;
+
+    return result;
+}
 
 export class MapComponent extends BaseController {
     constructor() {
