8b9223b906dc10aaa8af41990306584e4abf09f6
DarkLord <dfhrhfan12@yandex.ru>
2022-07-26 00:27:20 +0400

    Update getDamageByStrength

--- Task-10_Browser-game/src/ts/rpg/actions.ts
+++ Task-10_Browser-game/src/ts/rpg/actions.ts
@@ -5,7 +5,7 @@ export function getExpForStateLevelUp(state: RPG.TState): number {
 export function addStateExp(state: RPG.TState, exp: number) {
     state.experience += exp;
     const nextLevelExp = getExpForStateLevelUp(state);
-    if(state.experience >= nextLevelExp) {
+    if (state.experience >= nextLevelExp) {
         state.value++;
         state.experience = 0;
     }
@@ -15,12 +15,13 @@ export function getDamageByStrength(strength: number): TDiceExpression {
     const baseStrength = 10;
     const modToDice = 4;
     const result = { count: 1, dice: { min: 1, max: 6 }, mod: 0 };
-    let delta = strength - baseStrength;
-    if (delta < 0) {
-        result.mod = Math.sign(delta) * Math.round(Math.abs(delta) / 2);
+    const strengthMod = strength - baseStrength;
+    if (strengthMod < 0) {
+        result.mod = -Math.round(-strengthMod / 2);
     } else {
-        result.mod = delta;
-        while (result.mod > 2) {
+        result.mod = strengthMod % modToDice;
+        result.count = ((strengthMod - result.mod) / modToDice) + 1;
+        if (result.mod > (modToDice / 2)) {
             result.count += 1;
             result.mod -= modToDice;
         }
