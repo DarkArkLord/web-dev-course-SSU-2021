c3b966d9eec4b14d1490cc22a83f8e59d5af817a
DarkLord <dfhrhfan12@yandex.ru>
2022-08-20 21:00:54 +0400

    Add getCharacterWithLevel func

--- Task-10_Browser-game/src/ts/rpg/characters.ts
+++ Task-10_Browser-game/src/ts/rpg/characters.ts
@@ -1,4 +1,5 @@
-import { getPrimaryStatesTemplate, getCommonStates } from "./elements";
+import { getRandomValueWithProbability } from "../utils/random";
+import { States, getPrimaryStatesTemplate, getCommonStates } from "./elements";
 
 export function getCharacterTemplate(name: string): RPG.TCharacter {
     const primaryStates = getPrimaryStatesTemplate();
@@ -13,4 +14,27 @@ export function getCharacterTemplate(name: string): RPG.TCharacter {
     return character;
 }
 
-// add generate character by level
+export function getCharacterWithLevel(name: string, additionalLevels: number): RPG.TCharacter {
+    const primaryStates = getPrimaryStatesTemplate();
+
+    for (let i = 1; i < additionalLevels; i++) {
+        const probabilities = [
+            { probability: 1, value: States.Strength, },
+            { probability: 1, value: States.Dexterity, },
+            { probability: 1, value: States.Intelligence, },
+            { probability: 1, value: States.Constitution, },
+        ];
+        const state = getRandomValueWithProbability(probabilities);
+        primaryStates[state].value += 1;
+    }
+
+    const commonStates = getCommonStates(primaryStates);
+
+    const character = {
+        name,
+        primaryStates,
+        commonStates,
+    };
+
+    return character;
+}
