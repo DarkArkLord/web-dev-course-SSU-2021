fccf7d93ac50198b554453a302b9929f2fc75e98
DarkLord <dfhrhfan12@yandex.ru>
2022-08-20 22:47:26 +0400

    Upd BattleController

--- Task-10_Browser-game/src/ts/controllers/battleController.tsx
+++ Task-10_Browser-game/src/ts/controllers/battleController.tsx
@@ -1,7 +1,15 @@
 import { Commands } from "../controls";
+import { getCharacterWithLevel } from "../rpg/characters";
+import { renderBattleInfo } from "../rpg/characterToHtml";
 import { MenuComponent } from "./components/menuComponent";
 
+const CSS = {
+    table: 'width-100 align-center',
+};
+
 export class BattleController extends MenuComponent {
+    enemy: RPG.TCharacter;
+
     constructor(level: number) {
         const buttons = {
             back: {
@@ -13,6 +21,8 @@ export class BattleController extends MenuComponent {
         super([buttons.back], `Battle controller ${level}`);
         const instance = this;
 
+        this.enemy = getCharacterWithLevel(`ENEMY ${level}`, level)
+
         this.menuConfig.actions[buttons.back.value] = function () {
             instance.globalController.popController();
         }
@@ -20,5 +30,21 @@ export class BattleController extends MenuComponent {
         this.commandActions[Commands.Back] = function () {
             instance.globalController.popController();
         }
+    }
+    createElement(): HTMLElement {
+        const enemyTable = renderBattleInfo(this.enemy);
+        const characterTable = renderBattleInfo(this.globalController.gameData.character);
+        const headerTable = (<table class={CSS.table}>
+            <tr>
+                <td>
+                    {enemyTable}
+                </td>
+                <td>
+                    {characterTable}
+                </td>
+            </tr>
+        </table>) as Render.TChild;
+        this.menuConfig.header = headerTable;
+        return super.createElement();
     }
 }
