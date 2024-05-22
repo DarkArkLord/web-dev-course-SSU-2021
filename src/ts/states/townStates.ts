0d0cbf08e8b79bf3226c1acd9b3dfc0d070c4dae
DarkLord <dfhrhfan12@yandex.ru>
2024-05-22 22:55:48 +0400

    Add temple state

--- src/ts/states/townStates.ts
+++ src/ts/states/townStates.ts
@@ -36,36 +36,36 @@ export class TownMenuState extends BaseMenuState {
 
         super('Город', [townButtons.toMap, townButtons.toBattle, townButtons.temple, townButtons.states, townButtons.other]);
         const instance = this;
-        const gameData = instance.getData().getGameData();
+        const gameData = () => instance.getData().getGameData();
 
         // Настройка пунктов меню
         this.menuConfig.actions[townButtons.toMap.value] = function () {
-            gameData.saveGameData();
-            const lastLevel = gameData.getData().lastOpenMapLevel;
+            gameData().saveGameData();
+            const lastLevel = gameData().getData().lastOpenMapLevel;
             // const controller = new SelectMapLevelController(lastLevel);
-            // instance.statesController.pushState(controller);
+            // instance.getStatesController().pushState(controller);
         };
         this.menuConfig.actions[townButtons.toBattle.value] = function () {
-            gameData.saveGameData();
-            const lastLevel = gameData.getData().lastOpenMapLevel;
+            gameData().saveGameData();
+            const lastLevel = gameData().getData().lastOpenMapLevel;
             // const controller = new SelectBattleLevelController(lastLevel);
-            // instance.statesController.pushState(controller);
+            // instance.getStatesController().pushState(controller);
         };
         this.menuConfig.actions[townButtons.temple.value] = function () {
-            // const controller = new TempleMenuController();
-            // instance.statesController.pushState(controller);
+            const state = new TempleMenuController();
+            instance.getStatesController().pushState(state);
         };
         this.menuConfig.actions[townButtons.states.value] = function () {
-            const character = gameData.getData().character;
+            const character = gameData().getData().character;
             const characterTable = renderCharacter(character);
 
-            const controller = new InfoState([characterTable], InfoButtonsConfig.onlyBack, false);
+            const state = new InfoState([characterTable], InfoButtonsConfig.onlyBack, false);
 
-            instance.getStatesController().pushState(controller);
+            instance.getStatesController().pushState(state);
         };
         this.menuConfig.actions[townButtons.other.value] = function () {
-            const controller = new InfoState(['Другое'], InfoButtonsConfig.onlyBack, false);
-            instance.getStatesController().pushState(controller);
+            const state = new InfoState(['Другое'], InfoButtonsConfig.onlyBack, false);
+            instance.getStatesController().pushState(state);
         };
 
         // Настройка активности пунктов меню
@@ -73,7 +73,7 @@ export class TownMenuState extends BaseMenuState {
         townButtons.toBattle.isActive = activeOnPositiveHP;
 
         function activeOnPositiveHP() {
-            const player = gameData.getData().character;
+            const player = gameData().getData().character;
             const health = player.commonStates.health;
             return health.current > 0;
         }
@@ -82,6 +82,63 @@ export class TownMenuState extends BaseMenuState {
         this.commandActions[Commands.Back] = function () {
             const menuController = new MainMenuState();
             instance.getStatesController().pushState(menuController);
+        }
+    }
+}
+
+class SelectMapLevelState {
+    //
+}
+
+class SelectBattleLevelState {
+    //
+}
+
+class TempleMenuController extends BaseMenuState {
+    constructor() {
+        const templeButtons = {
+            heal: {
+                value: 'Исцеление',
+                description: () => 'Исцеление',
+                isActive: () => true,
+            },
+            back: {
+                value: 'Назад',
+                description: () => 'Назад',
+                isActive: () => true,
+            },
+        };
+
+        super('Город.Церковь', [templeButtons.heal, templeButtons.back]);
+        const instance = this;
+
+        // Настройка пунктов меню
+        this.menuConfig.actions[templeButtons.heal.value] = function () {
+            const player = instance.getData().getGameData().getData().character;
+            const hp = player.commonStates.health;
+            const healedHealth = hp.max - hp.current;
+            hp.current = hp.max;
+
+            const infoText = `Вылечено ${healedHealth} здоровья`;
+            const state = new InfoState([infoText], InfoButtonsConfig.onlyBack, false);
+
+            instance.getStatesController().pushState(state);
+        }
+
+        this.menuConfig.actions[templeButtons.back.value] = function () {
+            instance.getStatesController().popState();
+        }
+
+        // Настройка активности пунктов меню
+        templeButtons.heal.isActive = () => {
+            const player = instance.getData().getGameData().getData().character;
+            const health = player.commonStates.health;
+            return health.current < health.max;
+        };
+
+        // Кнопка назад - назад
+        this.commandActions[Commands.Back] = function () {
+            instance.getStatesController().popState();
         }
     }
 }
